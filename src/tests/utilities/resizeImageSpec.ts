b586a8a7b2e104bd4370c854ef18826a6f680c26
Ahmed Shaaban Ahmed <108435080+ahmedshaaban1608@users.noreply.github.com>
2022-10-26 12:28:00 +0200

    updated

--- src/tests/utilities/resizeImageSpec.ts
+++ src/tests/utilities/resizeImageSpec.ts
@@ -1,11 +1,11 @@
 import resize from '../../utilities/resizeImage';
 
 describe('Test image resizing', () => {
-  it('1- Expect to throw an error while resizing the image', async (): Promise<void> => {
+  it('1- Expect an error while resizing the image', async (): Promise<void> => {
     expect(await resize('river', '50', '50a')).toBe('Error is found');
   });
 
-  it('1- Expect to resize the image without throw an error', async (): Promise<void> => {
+  it('2- Expect to resize the image without error', async (): Promise<void> => {
     expect(await resize('river', '50', '50')).toBe('successfully resized');
   });
 });
